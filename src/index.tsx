ebfc1ced1c4943686a80f70ad20760571c291135
Kirill <k890484878789@gmail.com>
2023-10-02 17:09:35 +0500

    fix error

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import DataFilm from './types/data-film.tsx/data-film';
