1295d738ee1d132020fa59f105912ebff1809b64
Kirill <k890484878789@gmail.com>
2023-10-12 19:43:12 +0500

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,101 +1,15 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import App from './components/app/App';
-import DataFilm from './types/data-film.tsx/data-film';
-
-const films: ReadonlyArray<DataFilm> = [
-  {
-    name: 'The Grand Budapest Hotel poster',
-    pathImg: 'img/the-grand-budapest-hotel-poster.jpg',
-    genre: 'Drama',
-    date: '2014',
-  },
-  {
-    name: 'Fantastic Beasts: The Crimes of Grindelwald',
-    pathImg: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
-  },
-  {
-    name: 'Bohemian Rhapsody',
-    pathImg: 'img/bohemian-rhapsody.jpg',
-  },
-  {
-    name: 'Macbeth',
-    pathImg: 'img/macbeth.jpg',
-  },
-  {
-    name: 'Aviator',
-    pathImg: 'img/aviator.jpg',
-  },
-  {
-    name: 'We need to talk about Kevin',
-    pathImg: 'img/we-need-to-talk-about-kevin.jpg',
-  },
-  {
-    name: 'What We Do in the Shadows',
-    pathImg: 'img/what-we-do-in-the-shadows.jpg',
-  },
-  {
-    name: 'Revenant',
-    pathImg: 'img/revenant.jpg',
-  },
-  {
-    name: 'Johnny English',
-    pathImg: 'img/johnny-english.jpg',
-  },
-  {
-    name: 'Shutter Island',
-    pathImg: 'img/shutter-island.jpg',
-  },
-  {
-    name: 'Pulp Fiction',
-    pathImg: 'img/pulp-fiction.jpg',
-  },
-  {
-    name: 'No Country for Old Men',
-    pathImg: 'img/no-country-for-old-men.jpg',
-  },
-  {
-    name: 'Snatch',
-    pathImg: 'img/snatch.jpg',
-  },
-  {
-    name: 'Moonrise Kingdom',
-    pathImg: 'img/moonrise-kingdom.jpg',
-  },
-  {
-    name: 'Seven Years in Tibet',
-    pathImg: 'img/seven-years-in-tibet.jpg',
-  },
-  {
-    name: 'Midnight Special',
-    pathImg: 'img/midnight-special.jpg',
-  },
-  {
-    name: 'War of the Worlds',
-    pathImg: 'img/war-of-the-worlds.jpg',
-  },
-  {
-    name: 'Dardjeeling Limited',
-    pathImg: 'img/dardjeeling-limited.jpg',
-  },
-  {
-    name: 'Orlando',
-    pathImg: 'img/orlando.jpg',
-  },
-  {
-    name: 'Mindhunter',
-    pathImg: 'img/mindhunter.jpg',
-  },
-  {
-    name: 'Midnight Special',
-    pathImg: 'img/midnight-special.jpg',
-  },
-];
+import { films } from './mocks';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <App data={films}/>
+  <React.StrictMode>
+    <App data={films} />
+  </React.StrictMode>
 );
